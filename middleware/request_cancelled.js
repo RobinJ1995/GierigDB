dcc866799c1c26531528d5e5818fc77c01e1fb09
Robin Jacobs <robin@robinj.be>
2021-01-05 21:26:29 +0100

    log request cancellation

--- middleware/request_cancelled.js
+++ middleware/request_cancelled.js
@@ -1,6 +1,16 @@
 module.exports = (req, res, next) => {
+	const subscribers = [];
+	req.onCancel = cb => subscribers.push(cb);
+
 	req.on('close', () => {
 		req.cancelled = true;
+
+		if (res.headersSent) {
+			return;
+		}
+
+		console.log(`Request cancelled: ${req.method} ${req.originalUrl}`);
+		subscribers.forEach(cb => cb());
 	})
 
 	next();
